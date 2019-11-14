b35ec98dfe2b22ab9af2876d2e565c754a73cadc
HP <pchea99@gmail.com>
2019-11-14 16:26:31 +0700

    pk khmer calendar

--- rdb_calendar/lib/ui/about-us.dart
+++ rdb_calendar/lib/ui/about-us.dart
@@ -1,6 +1,7 @@
 import 'dart:ui';
 
 import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
 import 'package:rdb_calendar/res/fontsize.dart';
 import 'package:rdb_calendar/res/number.dart';
 import 'package:rdb_calendar/res/string.dart';
@@ -49,16 +50,13 @@ class _AboutUsState extends State<AboutUs> {
         crossAxisAlignment: CrossAxisAlignment.center,
         children: <Widget>[
           ImageLogo().imageLogo(),
-          Container(
-            padding: EdgeInsets.symmetric(horizontal: NumberRes.padding12),
-            width: MediaQuery.of(context).size.width,
-            child: Text(
-              "       ធនាគារអភិវឌ្ឍន៍ជនបទ (ធ.អ.ជ) គឺជាធនាគាររដ្ឋ ដែលបង្កើតឡើងដោយ រាជរដ្ឋាភិបាលកម្ពុជាដើម្បីចូលរួមអនុវត្ត គោលនយោបាយ របស់រាជរដ្ឋាភិបាល ក្នុងវិស័យកសិកម្ម និងសេដ្ឋកិច្ចជនបទ តាមរយៈការផ្តល់ ហិរញ្ញប្បទានដល់តួអង្គ ក្នុងខ្សែច្រវ៉ាក់ផលិតកម្មកសិកម្មរួមមាន អ្នកផលិត រហូតដល់ក្រុមហ៊ុនកែច្នៃនិងនាំចេញ ដែលអាចបំពេញនូវ កង្វះខាតរវាងតម្រូវការនិងការ ផ្គត់ផ្គង់សេវាហិរញ្ញវត្ថុ នៅតំបន់ជនបទសំដៅរួមចំណែក បង្កើនជីវភាពរស់នៅ របស់ប្រជាកសិករ និងចូលរួមអភិវឌ្ឍន៍សេដ្ឋកិច្ច សង្គម។",
-              style: TextStyle(
-                fontSize: FontSize.body1
-              ),
-              textAlign: TextAlign.justify,
+          Text(
+            "\t\t\tធនាគារអភិវឌ្ឍន៍ជនបទ (ធ.អ.ជ) គឺជាធនាគាររដ្ឋ ដែលបង្កើតឡើងដោយរាជរដ្ឋាភិបាលកម្ពុជា ដើម្បីចូលរួមអនុវត្ត គោលនយោបាយ របស់រាជរដ្ឋាភិបាលក្នុងវិស័យកសិកម្ម និងសេដ្ឋកិច្ចជនបទ តាមរយៈការផ្តល់ ហិរញ្ញប្បទានដល់ តួអង្គក្នុងខ្សែច្រវ៉ាក់ ផលិតកម្មកសិកម្ម រួមមាន អ្នកផលិត រហូតដល់ក្រុមហ៊ុនកែច្នៃ និងនាំចេញដែល អាចបំពេញនូវកង្វះខាត​​រវាងតម្រូវការ និងការផ្គត់ផ្គង់សេវាហិរញ្ញវត្ថុ នៅតំបន់ជនបទសំដៅរួមចំណែកបង្កើនជីវភាពរស់នៅ របស់ប្រជាកសិករ និងចូលរួម អភិវឌ្ឍន៍សេដ្ឋកិច្ច សង្គម។",
+            style: TextStyle(
+              fontSize: FontSize.body1,
             ),
+            textAlign: TextAlign.justify,
+            textDirection: TextDirection.ltr,
           ),
           SizedBox(height: NumberRes.width45)
         ],
