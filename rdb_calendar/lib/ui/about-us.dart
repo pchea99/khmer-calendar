98f7df8a8d2b36e962f827b388d6ff3daf4d5868
HP <pchea99@gmail.com>
2019-11-14 13:17:41 +0700

    pk khmer calendar

--- rdb_calendar/lib/ui/about-us.dart
+++ rdb_calendar/lib/ui/about-us.dart
@@ -1,7 +1,6 @@
 import 'dart:ui';
 
 import 'package:flutter/material.dart';
-import 'package:rdb_calendar/res/color.dart';
 import 'package:rdb_calendar/res/fontsize.dart';
 import 'package:rdb_calendar/res/number.dart';
 import 'package:rdb_calendar/res/string.dart';
