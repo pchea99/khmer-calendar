8954efa762d9658a4cb4981376ef475e5c7631ee
phakdey <pchea99@gmail.com>
2020-12-25 10:04:22 +0700

    pk: upgrade code rdb calendar ios

--- lib/res/string.dart
+++ lib/res/string.dart
@@ -1,10 +1,10 @@
 class StringRes {
-	static const String appName = "ប្រតិទិនខ្មែរ ធ.អ.ជ.ក";
+	static const String appName = "ប្រតិទិនខ្មែរ អេ អ ឌី ប៊ី (ARDB)";
 	static const String noConnectionKh = "គ្មានការភ្ជាប់អ៊ីនធឺណិត!";
 	static const String noConnectionEn = "No internet connection!";
 	static const String publicEvent = "ព្រឹត្តិការណ៍ប្រចាំឆ្នាំ";
 	static const String event = "ព្រឹត្តិការណ៍";
 	static const String home = "ទំព័រដើម";
-	static const String aboutUs = "អំពី អេអឌីប៊ី (ARDB)";
-	static const String contactUs = "ទំនាក់ទំនង ​អេអឌីប៊ី (ARDB)";
+	static const String aboutUs = "អំពី អេ អ ឌី ប៊ី (ARDB)";
+	static const String contactUs = "ទំនាក់ទំនង ​អេ អ ឌី ប៊ី (ARDB)";
 }
